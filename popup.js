fe3c2649d5f7fc7a0718cb46e3b56afded47a77a
callistajosette <145476238+callistajosette@users.noreply.github.com>
2024-03-27 15:11:08 -0500

    code

--- popup.js
+++ popup.js
@@ -1,1 +1,7 @@
-console.log(url);
+chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    // since only one tab should be active and in the current window at once
+    // the return variable should only have one entry
+    var activeTab = tabs[0];
+    var activeTabURL = activeTab.url; // or do whatever you need
+    alert(activeTabURL);
+  });
